c849db520568da90da09f60b5a24f5855884f8b3
cornsnakeftw <marianne@besquare.com.my>
2022-08-01 14:14:34 +0800

    grid

--- lib/Day 6widget.dart
+++ lib/Day 6widget.dart
@@ -1,85 +1,3 @@
-// import 'package:flutter/material.dart';
-// void main() {
-//   runApp(
-//     //the function runApp() starts the Flutter application
-//     // Directionality(
-//     //   textDirection: TextDirection.ltr,
-//     //   child: Center(
-//     //     child : MyStatefulWidget(),
-//     //  ), 
-//     // ), //Directionality
-
-//     MyStatelessWidget()
-//   );
-// }
-
-
-// class MyStatelessWidget extends StatelessWidget {
-  
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return  MaterialApp( title: 'Flutter Demo',
-//     theme: ThemeData( primarySwatch: Colors.blue,
-//     ),
-//     home: Homepage(),
-//     );
-//   }
-// }
-
-// class Homepage extends StatefulWidget {
-//   const Homepage({Key? key}) : super(key: key);
-
-//   @override
-//   State <Homepage> createState() =>  HomepageState();
-// }
-
-// class  HomepageState extends State <Homepage> {
-
-//   int count = 1;
-//   @override
-//   Widget build(BuildContext context) {
-//     // return Image.network('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'); ///return whatever to show at homepage
-//     return Scaffold(
-//         appBar: AppBar(
-//         title: Text("Appbar"),
-//         ),
-//                 body: Center(
-//           child:Text("Page $currentIndex is selected."),
-//           d),
-//           bottomNavigationBar : BottomNavigationBar(
-//             items: const [
-//               BottomNavigationBarItem(
-//                 icon: Icon (
-//                   Icons.home_outlined,
-//                 ),
-//                 label: 'Home',
-//                 ),
-//               BottomNavigationBarItem(
-//                 icon: Icon(
-//                   Icons.search,
-//                 ),
-//                 label: 'Search',
-//               )
-//               BottomNavigationBarItem(
-//                 icon: Icon(
-//                   Icons.call,
-//                 ),
-//                 label:'Call',
-//               )
-//             ]
-//           ));
-//         floatingActionButton:FloatingActionButton(
-//           child: Icon(Icons.add),
-//           onPressed: (){
-//             setState(() {
-//               count++;
-//             });
-//           },
-//         )
-//   }
-// }
-
 //----------------------------Day 6 Widget--------------\\
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
@@ -232,3 +150,249 @@ currentIndex = index;
 // // );
 }
 }
+
+
+//-------------------------------- List View ---------------------\\
+// import 'package:flutter/material.dart';
+// import 'package:flutter/rendering.dart';
+
+// void main() => runApp(App());
+
+// class App extends StatelessWidget {
+// const App({Key? key}) : super(key: key);
+
+// @override
+// Widget build(BuildContext context)
+// {
+// return MaterialApp(
+// home: HomePage(),
+// );
+// }
+// }
+
+// class HomePage extends StatefulWidget {
+// const HomePage({Key? key}) : super(key: key);
+
+// @override
+// State<HomePage> createState() => _HomePageState();
+// }
+
+// class _HomePageState extends State<HomePage> {
+// int count = 0;
+// int currentIndex = 0;
+
+// @override
+// Widget build(BuildContext context) {
+//   final List<String> entries = <String>[
+//     'First Item',
+//     'Second Item',
+//     'Third Item'
+//   ];
+//   final List<Color>colorCodes = <Color>[
+//   Colors.blue,
+//   Colors.red,
+//   Colors.yellow
+//   ];
+
+// return Scaffold(
+// appBar: AppBar(
+// title: Text('Appbar'),
+// centerTitle: true,
+// ),
+
+
+
+//   body: ListView.builder(
+//     itemCount: entries.length,
+//     itemBuilder: (BuildContext context, int index){
+//       return Container(
+//         height:50,
+//         color: colorCodes[index],
+//         child:Center(child:Text('${entries[index]}')),
+//     );
+//     },
+//     ),
+
+
+//-----------List View Seperated list --------------\\
+// import 'package:flutter/material.dart';
+// import 'package:flutter/rendering.dart';
+
+// void main() => runApp(App());
+
+// class App extends StatelessWidget {
+// const App({Key? key}) : super(key: key);
+
+// @override
+// Widget build(BuildContext context)
+// {
+// return MaterialApp(
+// home: HomePage(),
+// );
+// }
+// }
+
+// class HomePage extends StatefulWidget {
+// const HomePage({Key? key}) : super(key: key);
+
+// @override
+// State<HomePage> createState() => _HomePageState();
+// }
+
+// class _HomePageState extends State<HomePage> {
+// int count = 0;
+// int currentIndex = 0;
+
+// @override
+// Widget build(BuildContext context) {
+//   final List<String> entries = <String>[
+//     'First Item',
+//     'Second Item',
+//     'Third Item'
+//   ];
+//   final List<Color>colorCodes = <Color>[
+//   Colors.blue,
+//   Colors.red,
+//   Colors.yellow
+//   ];
+
+// return Scaffold(
+// appBar: AppBar(
+// title: Text('Appbar'),
+// centerTitle: true,
+// ),
+
+
+
+//   body: ListView.separated(
+//     itemCount: entries.length,
+//     itemBuilder: (BuildContext context, int index){
+//       return Container(
+//         height:50,
+//         color: colorCodes[index],
+//         child:Center(child:Text('${entries[index]}')),
+//     );
+//     },
+
+//     separatorBuilder:(BuildContext context,int index) =>const Divider(),
+//     ),
+//     floatingActionButton: FloatingActionButton(
+// child: Icon(Icons.add),
+// onPressed: () {
+// setState(() {
+// count++;
+// });
+// // child: Icon(Icons.remove),
+// },
+// ),
+
+// bottomNavigationBar: BottomNavigationBar(
+// items: const [
+// BottomNavigationBarItem(
+// icon: Icon(Icons.home,),
+// label: 'Home',
+// backgroundColor: Colors.red,
+// ),
+// BottomNavigationBarItem(icon: Icon(Icons.search,),
+// label: 'Search',
+// backgroundColor: Colors.blue,
+// ),
+// BottomNavigationBarItem(
+// icon: Icon(Icons.contact_phone,),
+// label: 'Contact',
+// backgroundColor: Colors.blue,
+// ),
+// ],
+// currentIndex: currentIndex,
+// onTap: (int index) {
+// setState(() {
+// currentIndex = index;
+// });
+// },
+
+// ),
+
+// );
+// //
+// // // );
+// }
+// }
+
+
+
+//-----------------GRIDVIEW -------------------\\
+// import 'package:flutter/material.dart';
+// import 'package:flutter/rendering.dart';
+
+// void main() => runApp(App());
+
+// class App extends StatelessWidget {
+//   const App({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: HomePage(),
+//     );
+//   }
+// }
+
+// class HomePage extends StatefulWidget {
+//   const HomePage({Key? key}) : super(key: key);
+
+//   @override
+//   State<HomePage> createState() => _HomePageState();
+// }
+
+// class _HomePageState extends State<HomePage> {
+//   int count = 0;
+//   int currentIndex = 0;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final List<String> entries = <String>[
+//       'First Item',
+//       'Second Item',
+//       'Third Item',
+//       'Fourth Item'
+//     ];
+
+//     final List<Color> colorCodes = <Color>[
+//       Colors.blue,
+//       Colors.red,
+//       Colors.yellow,
+//       Colors.green
+//     ];
+
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Appbar'),
+//         centerTitle: true,
+//       ),
+
+//       body: GridView.count(
+//         primary: false,
+//         padding: const EdgeInsets.all(16),
+//         crossAxisSpacing: 10,
+//         mainAxisSpacing: 20,
+//         crossAxisCount: 3,
+//         scrollDirection: Axis.vertical, //can remove to show non scrollable feature
+//         children: [
+//           Container(
+//             color: colorCodes[0],
+//             child: Text(entries[0]),
+//           ),
+//           Container(
+//             color: colorCodes[1],
+//             child: Text(entries[1]),
+//           ),
+//           Container(
+//             color: colorCodes[2],
+//             child: Text(entries[2]),
+//           ),
+//           Container(
+//             color: colorCodes[3],
+//             child: Text(entries[3]),
+//           ),
+//         ],
+//       ),
--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,14 @@
 //     //   textDirection: TextDirection.ltr,
 //     //   child: Center(
 //     //     child : MyStatefulWidget(),
-//     //  ), 
+//     //  ),
 //     // ), //Directionality
 
 //     MyStatelessWidget()
 //   );
 // }
 
-
 // class MyStatelessWidget extends StatelessWidget {
-  
 
 //   @override
 //   Widget build(BuildContext context) {
@@ -80,54 +78,92 @@
 //   }
 // }
 
-//----------------------------Day 6 Widget--------------\\
+//-----------------------------------------------Day 6 Widget------------------------------\\
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 void main() => runApp(App());
 
 class App extends StatelessWidget {
-const App({Key? key}) : super(key: key);
+  const App({Key? key}) : super(key: key);
 
-@override
-Widget build(BuildContext context)
-{
-return MaterialApp(
-home: HomePage(),
-);
-}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
 }
 
 class HomePage extends StatefulWidget {
-const HomePage({Key? key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
-@override
-State<HomePage> createState() => _HomePageState();
+  @override
+  State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-int count = 0;
-int currentIndex = 0;
+  int count = 0;
+  int currentIndex = 0;
 
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-appBar: AppBar(
-title: Text('Appbar'),
-centerTitle: true,
-),
+  @override
+  Widget build(BuildContext context) {
+    final List<String> entries = <String>[
+      'First Item',
+      'Second Item',
+      'Third Item',
+      'Fourth Item'
+    ];
+
+    final List<Color> colorCodes = <Color>[
+      Colors.blue,
+      Colors.red,
+      Colors.yellow,
+      Colors.green
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Appbar'),
+        centerTitle: true,
+      ),
+
+      body: GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(16),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 20,
+        crossAxisCount: 3,
+        scrollDirection: Axis.vertical,
+        children: [
+          Container(
+            color: colorCodes[0],
+            child: Text(entries[0]),
+          ),
+          Container(
+            color: colorCodes[1],
+            child: Text(entries[1]),
+          ),
+          Container(
+            color: colorCodes[2],
+            child: Text(entries[2]),
+          ),
+          Container(
+            color: colorCodes[3],
+            child: Text(entries[3]),
+          ),
+        ],
+      ),
 
 //-----------AspectRatio----------\\
 
-
 // body: /*const*/ AspectRatio(
-// aspectRatio: 100, 
+// aspectRatio: 100,
 //   child: Container(
 //     color: Colors.red,
 //     child: Text(
 //       'You have pushed the button this many times:'),
 // ),),
-
 
 //-------------------------ConstrainedBox--------------------------\\
 // body:ConstrainedBox(constraints: const BoxConstraints(maxWidth:500, maxHeight: 500),
@@ -139,11 +175,9 @@ centerTitle: true,
 //   'You have pushed the button this many times:',
 // ),),),
 
-
-
 //------------------------------SizedBox------------------------------\\
 // body: SizedBox(
- 
+
 //     width:500,
 //     child: Container(
 //     color: Colors.red,
@@ -151,7 +185,6 @@ centerTitle: true,
 //     ),
 //     ),
 // ),
-
 
 //-----------------Padding-----------\\
 // body: Padding(
@@ -173,62 +206,162 @@ centerTitle: true,
 //     constraints: const BoxConstraints(maxWidth: 50),
 //     ),
 
+      //--            Multiple-Child Widgets                   -\\
+      /// - can accept more than one widget as child
+      /// - non scrollable: Row, Column
+      //-------------------NON-Scrollable --Column--------------------\\
+      // body:Column (
+      //   mainAxisSize: MainAxisSize.min,
+      //   mainAxisAlignment: MainAxisAlignment.start,
+      //   children: <Widget>[
+      //   const Text( 'You have pushed', style:TextStyle(fontSize: 10)
+      //   ),
+      //   const Text(
+      //     'the buton this many times:',
+      //     ),
+      //   Expanded(child: Container(color:Colors.red)),
+      // ],
+      // ),
 
-  //--            Multiple-Child Widgets                   -\\
-  /// - can accept more than one widget as child
-  /// - non scrollable: Row, Column
-  
-  body:Column (
-    children: <Widget>[
-    const Text( 'You have pushed', style:TextStyle(fontSize: 30)
-    ),
-    const Text(
-      'the buton this many times:',
-      ),
-    Expanded(child: Container(color:Colors.red)),
-  ],
-  ),
+//---------------------SCROLLABLE --ListView--------------\\
+// body: ListView(
+//   children: <Widget>[
+//     Container(
+//       height: 50,
+//       color: Colors.blue,
+//       child: const Center(child:Text('First Item')),
+//     ),
+//     Container(
+//       height:50,
+//       color: Colors.blue,
+//       child: const Center(child: Text('Second Item')),
 
+//     ),
+//     Container(
+//       height: 50,
+//       color: Colors.blue,
+//       child: const Center(child: Text('Third Item')),
+//       ),
+//   ],
+// ),
 
+//-----------bring up to scaffold-------\\
+// @override
+// Widget build(Buildcontext context){
+//   final List<String> entries = <String>[
+//     'First Item',
+//     'Second Item',
+//     'Third Item'
+//   ];
 
-floatingActionButton: FloatingActionButton(
-child: Icon(Icons.add),
-onPressed: () {
-setState(() {
-count++;
-});
+//   final List<Color>colorCodes = <Color>[
+//   Colors.blue,
+//   Colors.red,
+//   Colors.yellow
+//   ];
+
+// }
+
+      ///---------to put under Scaffold-----\\
+      // body: ListView.builder(
+      //   itemCount: entries.length,
+      //   itemBuilder: (BuildContext context, int index){
+      //     return Container(
+      //       height:50,
+      //       color: colorCodes[index],
+      //       child:Center(child:Text('${entries[index]}')),
+      //   );
+      //   },
+      //   ),
+
+//--------------------SCROLLABLE  --GridView------\\
+
+// body: GridView.count(
+//   primary: false,
+//   padding: const EdgeInsets.all(16),
+//   crossAxisSpacing: 10,
+//   mainAxisSpacing: 10,
+//   crossAxisCount: 2,
+//   children: [
+//     Container(
+//       color: colorCodes[0],
+//       child: Text(entries[0]),
+//       ),
+//     Container(
+//       color: colorCodes[1],
+//       child: Text(entries[1]),
+//     ),
+//     Container(
+//       color: colorCodes[2],
+//       child:Text(entries[2]),
+//     ),
+//     Container(
+//       color: colorCodes[3],
+//       child: Text(entries[3]),
+//     ),
+//   ],
+// ),
+
+// @override
+// Widget build(BuildContext context){
+//   final List<String> entries = <String> [
+//     'First Item',
+//     'Second Item',
+//     'Third Item',
+//     'Fourth Item'
+//   ];
+
+//   final List<Color> colorCodes=<Color>[
+//     Colors.blue,
+//     Colors.red,
+//     Colors.yellow,
+//     Colors.green
+//   ];
+// }
+
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            count++;
+          });
 // child: Icon(Icons.remove),
-},
-),
+        },
+      ),
 
-bottomNavigationBar: BottomNavigationBar(
-items: const [
-BottomNavigationBarItem(
-icon: Icon(Icons.home,),
-label: 'Home',
-backgroundColor: Colors.red,
-),
-BottomNavigationBarItem(icon: Icon(Icons.search,),
-label: 'Search',
-backgroundColor: Colors.blue,
-),
-BottomNavigationBarItem(
-icon: Icon(Icons.contact_phone,),
-label: 'Contact',
-backgroundColor: Colors.blue,
-),
-],
-currentIndex: currentIndex,
-onTap: (int index) {
-setState(() {
-currentIndex = index;
-});
-},
-
-),
-
-);
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+            ),
+            label: 'Home',
+            backgroundColor: Colors.red,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+            ),
+            label: 'Search',
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.contact_phone,
+            ),
+            label: 'Contact',
+            backgroundColor: Colors.blue,
+          ),
+        ],
+        currentIndex: currentIndex,
+        onTap: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+      ),
+    );
 //
 // // );
-}
+  }
 }
